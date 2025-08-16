17b32d4f403a7d217a5f53dd6e825329a3011c10
Shanatanu Kale <kaleshantanu2260@gmail.com>
2025-08-16 18:57:47 +0530

    chore: Remove PrepEdge-clean directory

--- PrepEdge-clean/PrepEdgeBackend/src/main/java/com/PrepEdgeAi/PrepEdge/Repository/InterviewQuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.PrepEdgeAi.PrepEdge.Repository;
-
-
-
-import com.PrepEdgeAi.PrepEdge.Entity.InterviewQuestion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InterviewQuestionRepository extends JpaRepository<InterviewQuestion, Long> {
-}
